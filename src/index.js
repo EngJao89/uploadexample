f3b02778792d500ecd7908b25ccf9319838da8c0
EngJao89 <eng_joao@outlook.com>
2022-04-01 15:33:01 -0300

    Closes 03: "Implementa armazenamento no s3."

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+require("dotenv").config();
+
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
+
+module.exports = mongoose.connect(process.env.MONGO_URL,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use("/files", express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
 
 app.use(require('./routes'));
 
